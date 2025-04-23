d866d5ea3a905ae00bbbfc5f4519b62e96e6e72f
Guivserra <guilhermevserra04@gmail.com>
2025-04-23 19:49:51 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const instagramIcon = document.getElementById("instagram-icon");
+    if (instagramIcon) {
+      instagramIcon.addEventListener("click", function () {
+        window.open("https://www.instagram.com/conference_site/", "_blank");
+      });
+    }
+  });
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const instagramIcon = document.getElementById("youtube-icon");
+    if (instagramIcon) {
+      instagramIcon.addEventListener("click", function () {
+        window.open("https://www.youtube.com/watch?v=NU9L8hacRak&ab_channel=OeirasValley", "_blank");
+      });
+    }
+  });
+
